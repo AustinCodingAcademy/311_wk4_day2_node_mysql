be14f0741a133e92c5bc148b9a6f8e884677a414
Alex Munoz <anmunoz24@gmail.com>
2019-11-13 18:42:26 -0800

    update

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.66.47.15',
         user: 'root',
-        password: 'password',
+        password: '342819',
         database: 'admin'
       })
 
