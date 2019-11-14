19504232b40160a0ae534c5224f651b75797e806
galvanitic <develop@rjgalvan.com>
2019-11-13 19:56:14 -0600

    Rodolfo Galvan - Done

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.67.249.48',
         user: 'root',
-        password: 'password',
+        password: 'guanajuato1',
         database: 'admin'
       })
 
