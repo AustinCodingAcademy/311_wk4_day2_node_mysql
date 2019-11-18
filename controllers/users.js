c92241f1e24095b868c43679f7d1638694c59496
Your Name <you@example.com>
2019-11-18 14:21:38 -0600

    got the get by id working but hard coded though, let's make it dynamic

--- controllers/users.js
+++ controllers/users.js
@@ -12,21 +12,25 @@ const getAllUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
+  //let sql = 'SELECT FROM user WHERE id = ?'
+  //let sql = 'SELECT ??, ?? FROM ?? WHERE ?? = ?'
+const replacements = ['first_name', 'last_name', 'users', 'id', 1]
+let sql = 'SELECT ??, ?? FROM ?? WHERE ?? = ?'
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, replacements)
+  //sql = mysql.format(sql, [req.params.id])
 
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
-    return res.json(rows);
+    return res.json(rows[0]);
   })
 }
 
 const createUser = (req, res) => {
   // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
+  let sql = `INSERT INTO users WHERE first_name = ?, last_name = ?`
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = 'mysql.format(sql, req.params.${first_name[1], ${last_name[2]})'
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -36,9 +40,9 @@ const createUser = (req, res) => {
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
+  let sql = "UPDATE users SET first_name = ?, last_name = ? WHERE id = ?"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = `mysql.format(sql, ${first_name[1]}, ${last_name[2]})`
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
