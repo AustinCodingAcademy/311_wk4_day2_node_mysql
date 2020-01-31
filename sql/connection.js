52e92331ebd5257011d432842ffd9c056bf3939e
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-01-30 20:29:33 -0600

    did it but the last one

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
+        host: '35.188.48.136',
         user: 'root',
-        password: 'password',
+        password: 'Thefatlady3',
         database: 'admin'
       })
 
