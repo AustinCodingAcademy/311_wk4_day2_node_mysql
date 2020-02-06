3be70fa0cc7b3076e848ec5864692d0cab6e19f8
AndrewSimons3 <simdrew07@gmail.com>
2020-02-06 13:55:37 -0600

    finished

--- controllers/users.js
+++ controllers/users.js
@@ -56,7 +56,12 @@ const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
   let sql = "DELETE FROM ?? WHERE ?? = ?"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, ['users', 'first_name', req.params.first_name])
+  const replacements = [
+    'users',
+    'first_name',
+    req.params.first_name
+  ]
+  sql = mysql.format(sql, replacements)
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
