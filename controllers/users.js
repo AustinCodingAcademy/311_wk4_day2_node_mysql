12e3de4060529a8157f28342d8e05b317c158776
peter gilkey <peter_gilkey@yahoo.com>
2020-06-26 00:16:23 -0500

    Wk4Day2 complete

--- controllers/users.js
+++ controllers/users.js
@@ -4,17 +4,17 @@ const { handleSQLError } = require('../sql/error')
 
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
-  pool.query("SELECT * FROM users", (err, rows) => {
+  pool.query("SELECT * FROM users LIMIT 100", (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
   })
 }
 
 const getUserById = (req, res) => {
-  // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  let id = req.params.id;
+  let sql = "SELECT * FROM users WHERE id = ?"
+  
+  sql = mysql.format(sql, [id])
 
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
@@ -24,9 +24,11 @@ const getUserById = (req, res) => {
 
 const createUser = (req, res) => {
   // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  let firstName = req.body.first_name;
+  let lastName = req.body.last_name;
+  let sql = `INSERT into users (first_name, last_name) VALUES (?, ?)`
+
+  sql = mysql.format(sql, [firstName, lastName])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -36,9 +38,10 @@ const createUser = (req, res) => {
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
+  let id = req.params.id;
+  let sql = `UPDATE users SET first_name = ?, last_name = ? WHERE id = ${id}`
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, ["Jim", "Hopper"])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -48,9 +51,10 @@ const updateUserById = (req, res) => {
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let sql = ""
+  let firstName = req.params.first_name;
+  let sql = "DELETE from users WHERE first_name = ?"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, [firstName])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const bodyParser = require("body-parser");
 const usersRouter = require('./routers/users');
 
 const app = express();
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json())
-app.use('/users', usersRouter)
+app.use(usersRouter)
 
-app.get('/', (req, res) => {
-  res.send('Welcome to our server!')
-})
 
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
