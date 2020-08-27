95dfbf2154ad2404ac4b73acb98e62229c0cab75
TEldridge <skippydrumskins@yahoo.com>
2020-08-27 12:18:11 -0500

    get, get by id, create all working

--- routers/users.js
+++ routers/users.js
@@ -2,14 +2,15 @@ const express = require('express')
 const usersController = require('../controllers/users')
 const router = express.Router()
 
+
 router.get('/', usersController.getAllUsers)
 
 router.get('/:id', usersController.getUserById)
 
-router.post('/', usersController.createUser)
+router.post('/:first_name/:last_name', usersController.createUser)
 
 router.put('/:id', usersController.updateUserById)
 
-router.delete('/:first_name', usersController.deleteUserByFirstName)
+router.delete('/:first_name/:last_name', usersController.deleteUserByFirstName)
 
 module.exports = router
