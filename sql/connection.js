c8a6e76218d32f0fb27ad68c13f1231170f8abb0
Matt Carter <carter.matthew@me.com>
2021-01-04 21:43:15 -0500

    update

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
+        host: '35.231.162.78',
         user: 'root',
-        password: 'password',
+        password: 'Mc@121606',
         database: 'admin'
       })
 
