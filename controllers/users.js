5ac33c00ae59d7e4b4508dd06157fb632e3f0cf0
tcannetti <tcannetti@gmail.com>
2021-01-11 14:37:57 -0600

    first few sql statements done

--- controllers/users.js
+++ controllers/users.js
@@ -12,9 +12,9 @@ const getAllUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
+  let sql = "SELECT * FROM users WHERE id=?"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, [req.params.id])
 
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
@@ -24,7 +24,7 @@ const getUserById = (req, res) => {
 
 const createUser = (req, res) => {
   // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
+  let sql = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
   // WHAT GOES IN THE BRACKETS
   sql = mysql.format(sql, [])
 
@@ -36,7 +36,7 @@ const createUser = (req, res) => {
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
+  let sql = "UPDATE users SET first_name=?, last_name=? WHERE id=?"
   // WHAT GOES IN THE BRACKETS
   sql = mysql.format(sql, [])
 
