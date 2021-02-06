d9243208ebcb702ec6f2691ed014a54a57e90462
Stanley M Musial <68257825+StanleyMMusial@users.noreply.github.com>
2021-02-06 15:09:13 -0600

    Finished

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.70.168.242',
         user: 'root',
-        password: 'password',
+        password: 'rosita354',
         database: 'admin'
       })
 
