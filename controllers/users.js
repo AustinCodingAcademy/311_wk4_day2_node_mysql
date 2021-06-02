97a00f0419918ac30db163228eac5b07c2173dd5
JasonCornish5 <jason.cornish5@yahoo.com>
2021-06-02 08:06:34 -0500

    Final Push

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const { handleSQLError } = require('../sql/error')
 
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
-  pool.query("SELECT * FROM users", (err, rows) => {
+  pool.query("SELECT * FROM users join usersContact on users.id = usersContact.user_id join usersAddress on usersContact.user_id = usersAddress.user_id", (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
   })
@@ -12,10 +12,16 @@ const getAllUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
+  //let sql = "select * from users where id = ?"
+ // console.log(req.params.id)
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  //sql = mysql.format(sql, req.params.id)
 
+  let sql = 'select * from users where id = ?';
+
+  sql = mysql.format(sql, req.params.id)
+
+ 
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
@@ -23,22 +29,22 @@ const getUserById = (req, res) => {
 }
 
 const createUser = (req, res) => {
-  // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  
+  let sql = "insert into users (first_name, last_name) values (?, ?)"
+
+  sql = mysql.format(sql, [req.body.first_name, req.body.last_name]);
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
-    return res.json({ newId: results.insertId });
+    return res.json({ newId: results.insertId});
   })
 }
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
+  let sql = "update users set first_name = ?, last_name = ? where id = ?"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, [req.body.first_name, req.body.last_name, req.params.id])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -48,9 +54,9 @@ const updateUserById = (req, res) => {
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let sql = ""
+  let sql = "delete from users where first_name = ?"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, req.params.first_name)
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
